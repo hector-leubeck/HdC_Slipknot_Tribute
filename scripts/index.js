efdc10fb8ff6f6ae3673c74332f6c7f455d96e6f
hector-leubeck <hector.leubeck.lux.renatus@gmail.com>
2023-07-21 17:59:42 -0300

    feat: add history section

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,16 @@
-function showText(id) {
-    let checkState = document.getElementById(id);
-    if (checkState.style.display === none) {
-        checkState.style.display = "block"
-    } else {
-        checkState.style.display = "none"
-    }
-}
+window.addEventListener("load", function () {
+  const act = document.getElementsByClassName("collapsible-btn");
+
+  for (i = 0; i < act.length; i++) {
+    act[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+
+      let panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+});
