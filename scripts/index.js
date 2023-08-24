9d39e762a4c5fe688cfc27193f1a72d4601222b9
hector-leubeck <hector.leubeck.lux.renatus@gmail.com>
2023-08-23 22:02:25 -0300

    style: recreate history section

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,26 @@ window.addEventListener("load", function () {
 
   for (i = 0; i < act.length; i++) {
     act[i].addEventListener("click", function () {
+      const allPanels = document.querySelectorAll(".history-panel");
+      allPanels.forEach((panel) => {
+        console.log(panel);
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        }
+      });
+      const allButtons = document.querySelectorAll(".collapsible-btn");
+      allButtons.forEach((button) => {
+        button.classList.remove("active");
+      });
+      let panel = this.nextElementSibling;
+      let content = panel.nextElementSibling;
+      console.log(content);
       this.classList.toggle("active");
 
-      let panel = this.nextElementSibling;
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
       } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
+        panel.style.maxHeight = content.scrollHeight + "px";
       }
     });
   }
